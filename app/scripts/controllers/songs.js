3be3bfc5b4ea4a4f8be76394a0b9412a7c34dbca
Bram De Moor <bram.demoor@gmail.com>
2014-08-13 16:42:29 +0200

    Delete confirmation = done

--- app/scripts/controllers/songs.js
+++ app/scripts/controllers/songs.js
@@ -1,6 +1,6 @@
 'use strict';
 
-app.controller('SongsCtrl', function($scope, Song) {
+app.controller('SongsCtrl', function($scope, Song, $modal, $log) {
 
     $scope.songs = Song.all;
 
@@ -14,6 +14,39 @@ app.controller('SongsCtrl', function($scope, Song) {
     };
 
     $scope.deleteSong = function(songId) {
-        Song.delete(songId);
+
+        var modalInstance = $modal.open({
+            templateUrl: 'myModalContent.html',
+            controller: ModalInstanceCtrl,
+            size: 'sm',
+            resolve: {
+                song: function () {
+                    return songId;
+                }
+            }
+        });
+
+        modalInstance.result.then(
+            function (selectedItem) {
+                Song.delete(songId);
+            }, function () {
+                $log.info('Modal dismissed at: ' + new Date());
+            });
     };
 });
+
+// Please note that $modalInstance represents a modal window (instance) dependency.
+// It is not the same as the $modal service used above.
+
+var ModalInstanceCtrl = function ($scope, $modalInstance, song) {
+
+    $scope.song = song;
+
+    $scope.yes = function () {
+        $modalInstance.close();
+    };
+
+    $scope.no = function () {
+        $modalInstance.dismiss('cancel');
+    };
+};
