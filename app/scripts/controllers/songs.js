17a38051997617bada63923c99a12d9283c2cc71
Bram De Moor <bram.demoor@gmail.com>
2014-08-13 16:46:04 +0200

    Modal refactor

--- app/scripts/controllers/songs.js
+++ app/scripts/controllers/songs.js
@@ -4,7 +4,6 @@ app.controller('SongsCtrl', function($scope, Song, $modal, $log) {
 
     $scope.songs = Song.all;
 
-    // NavVfpp-1L4
     $scope.song = { param: '', artist: '', title: '' };
 
     $scope.submitSong = function() {
@@ -15,38 +14,19 @@ app.controller('SongsCtrl', function($scope, Song, $modal, $log) {
 
     $scope.deleteSong = function(songId) {
 
-        var modalInstance = $modal.open({
+        $modal.open({
             templateUrl: 'myModalContent.html',
-            controller: ModalInstanceCtrl,
+            controller: function ($scope, $modalInstance, song) {
+                $scope.song = song;
+                $scope.yes = function () { $modalInstance.close(); };
+                $scope.no = function () { $modalInstance.dismiss('cancel'); };
+            },
             size: 'sm',
             resolve: {
-                song: function () {
-                    return songId;
-                }
+                song: function () { return songId; }
             }
-        });
-
-        modalInstance.result.then(
-            function (selectedItem) {
-                Song.delete(songId);
-            }, function () {
-                $log.info('Modal dismissed at: ' + new Date());
-            });
+        }).result.then(
+            function (selectedItem) { Song.delete(songId); },
+            function () { $log.info('Modal dismissed at: ' + new Date()); });
     };
 });
-
-// Please note that $modalInstance represents a modal window (instance) dependency.
-// It is not the same as the $modal service used above.
-
-var ModalInstanceCtrl = function ($scope, $modalInstance, song) {
-
-    $scope.song = song;
-
-    $scope.yes = function () {
-        $modalInstance.close();
-    };
-
-    $scope.no = function () {
-        $modalInstance.dismiss('cancel');
-    };
-};
