b9511d0f5798b6f71dd4cd8b85777c6eec9fa821
Bram De Moor <bram.demoor@gmail.com>
2014-08-14 14:26:12 +0200

    Module dependency unit tests

--- test/spec/appSpec.js
+++ test/spec/appSpec.js
@@ -12,6 +12,25 @@ describe("SOTD: Testing Modules", function() {
            expect(module).toBeDefined();
         });
 
+
+        describe("Dependencies:", function() {
+
+            var deps;
+            var hasModule = function(m) {
+                return deps.indexOf(m) >= 0;
+            };
+            beforeEach(function() {
+                deps = module.value('angSotdApp').requires;
+            });
+
+            it("should have firebase as a dependency", function() {
+                expect(hasModule('firebase')).toEqual(true);
+            });
+
+            it("should not have I_DO_NOT_EXIST as a dependency", function() {
+                expect(hasModule('I_DO_NOT_EXIST')).toEqual(false);
+            });
+        });
     });
 
 });
